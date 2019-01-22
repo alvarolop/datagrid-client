f0076d6942652f8201077d4935f81ba8ced5c1cd
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2019-01-22 17:27:57 +0100

    Add client-spring-basic example

---
+++ client-spring-basic/src/main/java/com/example/clientdatagrid/FootballManager.java
@@ -0,0 +1,131 @@
+package com.example.clientdatagrid;
+
+import java.io.Console;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.infinispan.client.hotrod.configuration.Configuration;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Martin Gencur
+ */
+//@Configuration
+//@PropertySource("classpath:application.properties")
+//@ConfigurationProperties(prefix="jdg.hotrod")
+public class FootballManager {
+	
+    private String host = "node1.dg.vm";
+    private int port = 11222;
+
+    private static final String msgTeamMissing = "The specified team \"%s\" does not exist, choose next operation\n";
+    private static final String msgEnterTeamName = "Enter team name: ";
+
+    private static final String teamsKey = "teams";
+
+    private Console con;
+    private RemoteCacheManager cacheManager;
+    private RemoteCache<String, Object> cache;
+
+    public FootballManager(Console con) {
+        this.con = con;
+        Configuration configuration = new ConfigurationBuilder()
+        		.addServer()
+        			.host(host)
+        			.port(port)
+        		.build();
+        cacheManager = new RemoteCacheManager(configuration);
+        cache = cacheManager.getCache("teams");
+        
+        
+        if(!cache.containsKey(teamsKey)) {
+            List<String> teams = new ArrayList<String>();
+            Team t = new Team("Barcelona");
+            t.addPlayer("Messi");
+            t.addPlayer("Pedro");
+            t.addPlayer("Puyol");
+            cache.put(t.getName(), t);
+            teams.add(t.getName());
+            cache.put(teamsKey, teams);
+        }
+    }
+
+    public void addTeam() {
+        String teamName = con.readLine(msgEnterTeamName);
+        @SuppressWarnings("unchecked")
+        List<String> teams = (List<String>) cache.get(teamsKey);
+        if (teams == null) {
+            teams = new ArrayList<String>();
+        }
+        Team t = new Team(teamName);
+        cache.put(teamName, t);
+        teams.add(teamName);
+        // maintain a list of teams under common key
+        cache.put(teamsKey, teams);
+    }
+
+    public void addPlayers() {
+        String teamName = con.readLine(msgEnterTeamName);
+        String playerName = null;
+        Team t = (Team) cache.get(teamName);
+        if (t != null) {
+            while (!(playerName = con.readLine("Enter player's name (to stop adding, type \"q\"): ")).equals("q")) {
+                t.addPlayer(playerName);
+            }
+            cache.put(teamName, t);
+        } else {
+            con.printf(msgTeamMissing, teamName);
+        }
+    }
+
+    public void removePlayer() {
+        String playerName = con.readLine("Enter player's name: ");
+        String teamName = con.readLine("Enter player's team: ");
+        Team t = (Team) cache.get(teamName);
+        if (t != null) {
+            t.removePlayer(playerName);
+            cache.put(teamName, t);
+        } else {
+            con.printf(msgTeamMissing, teamName);
+        }
+    }
+
+    public void removeTeam() {
+        String teamName = con.readLine(msgEnterTeamName);
+        Team t = (Team) cache.get(teamName);
+        if (t != null) {
+            cache.remove(teamName);
+            @SuppressWarnings("unchecked")
+            List<String> teams = (List<String>) cache.get(teamsKey);
+            if (teams != null) {
+                teams.remove(teamName);
+            }
+            cache.put(teamsKey, teams);
+        } else {
+            con.printf(msgTeamMissing, teamName);
+        }
+    }
+
+    public void printTeams() {
+        @SuppressWarnings("unchecked")
+        List<String> teams = (List<String>) cache.get(teamsKey);
+        if (teams != null) {
+            for (String teamName : teams) {
+                con.printf(cache.get(teamName).toString());
+            }
+        }
+    }
+
+    public void stop() {
+        cacheManager.stop();
+    }
+}
