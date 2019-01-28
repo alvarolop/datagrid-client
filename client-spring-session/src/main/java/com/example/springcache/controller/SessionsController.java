0824bb45b38e74688336c30596e4178618ae1699
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2019-01-28 17:47:41 +0100

    Added some methods to request specific information about sessions

--- client-spring-session/src/main/java/com/example/springcache/controller/SessionsController.java
+++ client-spring-session/src/main/java/com/example/springcache/controller/SessionsController.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.spring.provider.SpringRemoteCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.session.MapSession;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +19,7 @@ public class SessionsController {
    @Autowired
    SpringRemoteCacheManager cacheManager;
 
-   @RequestMapping("/sessions")
+   @GetMapping("/sessions")
    public Map<String, String> session(HttpServletRequest request) {
       Map<String, String> result = new HashMap<>();
       String sessionId = request.getSession(true).getId();
@@ -24,5 +27,27 @@ public class SessionsController {
       // By default Infinispan integration for Spring Session will use 'sessions' cache.
       result.put("active:", cacheManager.getCache("sessions").getNativeCache().keySet().toString());
       return result;
+   }
+   
+   @GetMapping("/session")
+   String getSessionId(HttpSession session) {
+       return session.getId();
+   }
+   
+   @SuppressWarnings("unchecked")
+   @GetMapping("/sessionattributes")
+   String getSessionAttributes(HttpSession session) {
+	   // https://docs.spring.io/spring-session/docs/current/api/org/springframework/session/MapSession.html
+	   RemoteCache<String, MapSession> cache = (RemoteCache<String, MapSession>) cacheManager.getCache("sessions").getNativeCache();
+	   MapSession mapSession = cache.get(session.getId());
+       return mapSession.getAttributeNames().toString();
+   }
+   @SuppressWarnings("unchecked")
+   @GetMapping("/sessionexpired")
+   String isSessionExpired(HttpSession session) {
+	   // https://docs.spring.io/spring-session/docs/current/api/org/springframework/session/MapSession.html
+	   RemoteCache<String, MapSession> cache = (RemoteCache<String, MapSession>) cacheManager.getCache("sessions").getNativeCache();
+	   MapSession mapSession = cache.get(session.getId());
+       return Boolean.toString(mapSession.isExpired());
    }
 }
