78ff526156642732c27cb21fb80fbd9e8012c1f0
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2019-02-27 11:09:25 +0100

    Added skeleton for spring boot applications that will run in pods to test connections

---
+++ client-ocp-hotrod-73/src/main/java/me/ignaciosanchez/hotrodtester/datagrid/Config.java
@@ -0,0 +1,38 @@
+package me.ignaciosanchez.hotrodtester.datagrid;
+
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.spring.provider.SpringRemoteCacheManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class Config {
+
+    @Value("${datagrid.host}")
+    private String host;
+
+    @Value("${datagrid.port}")
+    private String port;
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new SpringRemoteCacheManager(infinispanCacheManager());
+    }
+
+    @Bean
+    public RemoteCacheManager infinispanCacheManager() {
+
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.addServer()
+                .host(host)
+                .port(Integer.parseInt(port));
+        //builder.nearCache().mode(NearCacheMode.INVALIDATED).maxEntries(100);
+
+        return new RemoteCacheManager(builder.build());
+    }
+}
