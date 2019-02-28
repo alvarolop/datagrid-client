bf5a7b4484c153140d1583cc817e67b29ba986d8
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2019-02-28 12:11:41 +0100

    Test

--- client-ocp-hotrod-73/src/main/java/me/ignaciosanchez/hotrodtester/datagrid/Config.java
+++ client-ocp-hotrod-73/src/main/java/me/ignaciosanchez/hotrodtester/datagrid/Config.java
@@ -3,8 +3,9 @@ package me.ignaciosanchez.hotrodtester.datagrid;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.spring.remote.provider.SpringRemoteCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,6 +32,7 @@ public class Config {
     @Value("${datagrid.password}")
     private String password;
     
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Bean
     public SpringRemoteCacheManager cacheManager() {
@@ -39,35 +41,35 @@ public class Config {
 
     @Bean
     public RemoteCacheManager infinispanCacheManager() {
-    	
-		String value = "Connection to: " + host + " and port " + port + " with security (" + authentication.contains("true") +  "): "
-				+ username + " / " + password + " / " + serverName + ".\n";
 		
-		System.out.println(value);
+		log.info("-------> Connection to: " + host + " and port " + port);
+		log.info("-------> Security " + authentication.contains("true") + ": " + username + " / " + password + " / " + serverName);
 
         ConfigurationBuilder builder = new ConfigurationBuilder();
         builder.addServer()
                 .host(host)
                 .port(Integer.parseInt(port));
-    // https://access.redhat.com/documentation/en-us/red_hat_data_grid/7.3/html-single/red_hat_data_grid_user_guide/#hotrod_java_client
-        if (authentication.contains("true")) {
-        	// Option 1
-//            builder.security()
-//        	.authentication()
-//        		.saslMechanism("DIGEST-MD5")
-//		        .username(username)
-//		        .password(password)
-//		        .realm("ApplicationRealm");
-            // Option 2
-//			builder.security()
-//			.authentication()
-//				.enable()
-//				.serverName(serverName)
-//				.saslMechanism("DIGEST-MD5")
-//				.callbackHandler(new CacheCallbackHandler(username, "ApplicationRealm", password.toCharArray()));
-        }
         //builder.nearCache().mode(NearCacheMode.INVALIDATED).maxEntries(100);
 
         return new RemoteCacheManager(builder.build());
     }
 }
+
+
+// https://access.redhat.com/documentation/en-us/red_hat_data_grid/7.3/html-single/red_hat_data_grid_user_guide/#hotrod_java_client
+//if (authentication.contains("true")) {
+	// Option 1
+//  builder.security()
+//	.authentication()
+//		.saslMechanism("DIGEST-MD5")
+//      .username(username)
+//      .password(password)
+//      .realm("ApplicationRealm");
+  // Option 2
+//	builder.security()
+//	.authentication()
+//		.enable()
+//		.serverName(serverName)
+//		.saslMechanism("DIGEST-MD5")
+//		.callbackHandler(new CacheCallbackHandler(username, "ApplicationRealm", password.toCharArray()));
+//}
