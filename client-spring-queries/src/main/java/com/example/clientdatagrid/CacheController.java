6d6ea06747463dd0234ab726e0590c7b882f0a23
Alvaro Lopez Medina <alopezme@redhat.com>
2019-06-12 18:02:28 +0200

    Minimum modifications

--- client-spring-queries/src/main/java/com/example/clientdatagrid/CacheController.java
+++ client-spring-queries/src/main/java/com/example/clientdatagrid/CacheController.java
@@ -1,13 +1,5 @@
 package com.example.clientdatagrid;
 
-import org.infinispan.client.hotrod.DataFormat;
-import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.client.hotrod.RemoteCacheManager;
-
-import org.infinispan.client.hotrod.configuration.Configuration;
-import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-import org.infinispan.commons.dataconversion.MediaType;
-import org.infinispan.commons.marshall.UTF8StringMarshaller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.clientdatagrid.Team;
 
 /**
  * @author Martin Gencur
